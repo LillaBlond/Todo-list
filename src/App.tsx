dd8b45dc5c57cc473153256b7bb7dd1f4841aab3
Annika Karlsson <en_anka@hotmail.com>
2025-01-16 19:35:00 +0000

    Added showTaskList component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./global.css";
 import { AddTask } from "./components/AddTask";
+import { ShowTaskList } from "./components/ShowTaskList";
 
 function App() {
   return <>
   <h1>Todo List</h1>
     <AddTask></AddTask>
+    <ShowTaskList></ShowTaskList>
     </>;
 }
 
