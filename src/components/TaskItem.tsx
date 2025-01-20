68328d994a764e07f4c292b573a4101b51bc0550
Annika Karlsson <en_anka@hotmail.com>
2025-01-20 16:45:00 +0000

    Can edit a task, save or cancel change

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,8 +1,8 @@
 import "../css/Global.css";
 import "../css/TaskItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan, faPen, faFire} from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react";
+import { faTrashCan, faPen, faFire, faFloppyDisk, faXmark} from "@fortawesome/free-solid-svg-icons"
+import { ChangeEvent, useState } from "react";
 
 interface ITaskItemProps{
     id: number;
@@ -13,16 +13,33 @@ interface ITaskItemProps{
 
 export function TaskItem({id, content, removeItem}: ITaskItemProps){
     const [isDone, setIsDone] = useState<boolean>(false);
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const [inputValue, setInputValue] = useState(content);
+    const [tempValue, setTempValue] = useState("");
 
-    function changeStatus(){
+    function changeProgressStatus(){
         isDone ? setIsDone(false) : setIsDone(true);
     }
 
+    function changeActiveStatus(){
+        isActive ? setIsActive(false) : setIsActive(true);  
+        setTempValue(inputValue);
+    }
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>){
+        setInputValue(e.target.value);
+    }
+
+    function cancelChange(){
+        setInputValue(tempValue);
+        changeActiveStatus();
+    }
+
     return <li key={id} className={"taskItem" + " " +(isDone ? "done" : "")}>
-        <input type="checkbox" name="checkbox" onClick={changeStatus}/>
-        <input type="text" className={(isDone ? "done" : "")} value={content} disabled/>
-        <FontAwesomeIcon className="icon" icon={faFire} />
-        <FontAwesomeIcon className="icon" icon={faTrashCan} onClick={() => removeItem(id)} />
-        <FontAwesomeIcon className="icon" icon={faPen} />
+        {!isDone && <FontAwesomeIcon className="icon" icon={faFire} />}
+        <input type="checkbox" name="checkbox" onClick={changeProgressStatus} disabled={isActive ? true : false}/>
+        <input type="text" className={(isDone ? "done" : "")} value={inputValue} onChange={handleChange} disabled={isActive ? false : true}/>
+        {!isDone && <FontAwesomeIcon className="icon" icon={isActive ? faFloppyDisk: faPen} onClick={ () => changeActiveStatus()}/>}
+        <FontAwesomeIcon className="icon" icon={isActive ? faXmark: faTrashCan} onClick={() => { !isActive ? removeItem(id) : cancelChange()} } />
     </li>
 }
